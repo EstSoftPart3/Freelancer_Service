a8082f40f1ddbdfac5b3cae24fe9f7ea68b65136
Analog-PaulBaek <qkdnf325@naver.com>
2025-07-21 15:03:19 +0900

    fix : CORS 설정 완료 / 외부 접속 테스트 완료

--- backend/src/main/java/com/example/demo/config/SecurityConfigDev.java
+++ backend/src/main/java/com/example/demo/config/SecurityConfigDev.java
@@ -1,9 +1,7 @@
 package com.example.demo.config;
 
-import com.example.demo.domain.user.util.JwtAuthenticationFilter;
-import com.example.demo.domain.user.util.JwtProvider;
-import jakarta.servlet.Filter;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,6 +13,12 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.example.demo.domain.user.util.JwtAuthenticationFilter;
+import com.example.demo.domain.user.util.JwtProvider;
+
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
+
 @Configuration
 @Profile("dev")
 @RequiredArgsConstructor
@@ -22,14 +26,36 @@ public class SecurityConfigDev {
 
     private final JwtProvider jwtProvider;
 
+    // 개발용
+    // @Bean
+    // public CorsConfigurationSource corsConfigurationSource() {
+    // CorsConfiguration configuration = new CorsConfiguration();
+    // configuration.addAllowedOrigin("http://localhost:8504");
+    // configuration.addAllowedOrigin("http://localhost:3000");
+    // configuration.addAllowedOrigin("https://31f05472343a.ngrok-free.app");
+    // configuration.addAllowedOrigin("https://test-paulbaeks-projects.vercel.app");
+    // configuration.addAllowedMethod("*");
+    // configuration.addAllowedHeader("*");
+    // configuration.setAllowCredentials(true); // 쿠키 허용
+
+    // UrlBasedCorsConfigurationSource source = new
+    // UrlBasedCorsConfigurationSource();
+    // source.registerCorsConfiguration("/**", configuration);
+    // return source;
+    // }
+
+    // 외부 배포 테스트용
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:8504");
-        configuration.addAllowedOrigin("http://localhost:3000");
-        configuration.addAllowedMethod("*");
-        configuration.addAllowedHeader("*");
-        configuration.setAllowCredentials(true); // 쿠키 허용
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:8504",
+                "http://localhost:3000",
+                "https://e4f5e44bf928.ngrok-free.app",
+                "https://test-eight-tau-87.vercel.app/"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
@@ -39,7 +65,8 @@ public class SecurityConfigDev {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors().and()
+                // .cors().and() // 개발용
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // 외부 배포 테스트용
                 .csrf().disable()
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
