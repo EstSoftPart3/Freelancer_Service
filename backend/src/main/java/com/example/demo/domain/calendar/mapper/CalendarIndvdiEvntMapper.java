b38a58ee2cc91c28bc3f934c4adf372e83c66f5e
leejaeseok-98 <wotjrdl98@naver.com>
2025-10-12 22:21:15 +0900

    Revert "Feature/calendar scrap project view"

--- backend/src/main/java/com/example/demo/domain/calendar/mapper/CalendarIndvdiEvntMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.domain.calendar.mapper;
-
-import com.example.demo.domain.calendar.entity.CalendarIndvdiEvnt;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface CalendarIndvdiEvntMapper {
-    void insert(CalendarIndvdiEvnt indvdiEvnt);
-}
