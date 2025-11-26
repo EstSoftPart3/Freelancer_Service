5fa10f3d752b69f26b4bdbfb1f85a5235c3ae7b4
DESKTOP-O11N0GQ\estsoft <123>
2025-11-26 15:30:49 +0900

    fix: 이력서 등록 날짜 포맷 문제 해결 #98

--- frontend/vue_js/src/fo/components/mypage/personal/ResumeCompanyModal.vue
+++ frontend/vue_js/src/fo/components/mypage/personal/ResumeCompanyModal.vue
@@ -171,7 +171,7 @@ const submit = () => {
       const year = dateString.getFullYear()
       const month = String(dateString.getMonth() + 1).padStart(2, '0')
       const day = String(dateString.getDate()).padStart(2, '0')
-      return `${year}.${month}.${day}`
+      return `${year}-${month}-${day}`
     }
     return dateString
   }
--- frontend/vue_js/src/fo/components/mypage/personal/TrainingModal.vue
+++ frontend/vue_js/src/fo/components/mypage/personal/TrainingModal.vue
@@ -142,7 +142,7 @@ const submit = () => {
       const year = dateString.getFullYear()
       const month = String(dateString.getMonth() + 1).padStart(2, '0')
       const day = String(dateString.getDate()).padStart(2, '0')
-      return `${year}.${month}.${day}`
+      return `${year}-${month}-${day}`
     }
     return dateString
   }
