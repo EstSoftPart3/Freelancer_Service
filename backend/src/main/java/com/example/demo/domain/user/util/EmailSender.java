a3f2aa1e867c981348b90591097346889e2bf86f
DESKTOP-I05SQM2\신중현 <joungheon15@gmail.com>
2026-02-13 10:13:51 +0900

    fix : 이메일 설정 추가

--- backend/src/main/java/com/example/demo/domain/user/util/EmailSender.java
+++ backend/src/main/java/com/example/demo/domain/user/util/EmailSender.java
@@ -1,6 +1,7 @@
 package com.example.demo.domain.user.util;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
@@ -14,9 +15,13 @@ public class EmailSender {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+    
     public void send(String to, String subject, String text) throws MessagingException {
         MimeMessage message = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom(fromEmail);
         helper.setTo(to);
         helper.setSubject(subject);
         helper.setText(text, true);
