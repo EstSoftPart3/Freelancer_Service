98d66b8d604135cadbf1dba485a9dcd8ae6f0464
DESKTOP-I05SQM2\신중현 <joungheon15@gmail.com>
2026-02-27 14:05:37 +0900

    feat: 관리자 대시보드 서비스 생성

---
+++ backend/src/main/java/com/example/demo/domain/admin/service/AdminDashBoardService.java
@@ -0,0 +1,12 @@
+package com.example.demo.domain.admin.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AdminDashBoardService {
+	
+	
+}
