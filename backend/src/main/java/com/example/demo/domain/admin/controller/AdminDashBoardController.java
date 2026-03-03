9f65d72d7a7cf25823f12fe19d30bd57e9fd7dfa
DESKTOP-I05SQM2\신중현 <joungheon15@gmail.com>
2026-03-03 09:38:47 +0900

    feat: 대시보드 조회 컨트롤러 구현

--- backend/src/main/java/com/example/demo/domain/admin/controller/AdminDashBoardController.java
+++ backend/src/main/java/com/example/demo/domain/admin/controller/AdminDashBoardController.java
@@ -1,5 +1,7 @@
 package com.example.demo.domain.admin.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.common.ApiResponse;
+import com.example.demo.domain.admin.dto.response.DayStatsDTO;
+import com.example.demo.domain.admin.dto.response.LatestPostsDTO;
+import com.example.demo.domain.admin.dto.response.SummaryDTO;
 import com.example.demo.domain.admin.service.AdminDashBoardService;
 import com.example.demo.domain.user.service.LoginService;
 
@@ -18,4 +23,24 @@ import lombok.RequiredArgsConstructor;
 public class AdminDashBoardController {
 	private final AdminDashBoardService adminDashBoardService;
 	
+	@GetMapping("/weekly")
+	public ResponseEntity<ApiResponse<List<DayStatsDTO>>> getWeeklyData(){
+		List<DayStatsDTO> weeklyStats = adminDashBoardService.getWeeklyStats();
+		
+		return ResponseEntity.ok(ApiResponse.of(HttpStatus.OK, "최근 7일간 목록 조회 성공", weeklyStats));
+	}
+	
+	@GetMapping("/summary")
+	public ResponseEntity<ApiResponse<List<SummaryDTO>>> getDayData() {
+		List<SummaryDTO> dayStats = adminDashBoardService.getDayStats();
+		
+		return ResponseEntity.ok(ApiResponse.of(HttpStatus.OK, "일일 목록 조회 성공", dayStats));
+	}
+	
+	@GetMapping("/latestpost")
+	public ResponseEntity<ApiResponse<List<LatestPostsDTO>>> getLatestPostsData() {
+		List<LatestPostsDTO> latestPostsList = adminDashBoardService.getLatestPosts();
+		
+		return ResponseEntity.ok(ApiResponse.of(HttpStatus.OK, "최근 5개 게시글 조회 성공", latestPostsList));
+	}
 }
