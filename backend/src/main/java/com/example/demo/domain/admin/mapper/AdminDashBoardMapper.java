db697e83d6970d6a243dc6919a6a83dd10b47e22
DESKTOP-I05SQM2\신중현 <joungheon15@gmail.com>
2026-03-03 09:34:15 +0900

    feat: 대시보드 통계/로그 매퍼 생성

--- backend/src/main/java/com/example/demo/domain/admin/mapper/AdminDashBoardMapper.java
+++ backend/src/main/java/com/example/demo/domain/admin/mapper/AdminDashBoardMapper.java
@@ -1,8 +1,36 @@
 package com.example.demo.domain.admin.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
-@Mapper
-public class AdminDashBoardMapper {
+import com.example.demo.domain.admin.dto.DateCountDTO;
+import com.example.demo.domain.admin.dto.response.LatestPostsDTO;
 
+@Mapper
+public interface AdminDashBoardMapper {
+	
+	List<DateCountDTO> getWeeklyConnectedUserCount();
+	
+	List<DateCountDTO> getWeeklyProjectCount();
+	
+	List<DateCountDTO> getWeeklyJobCount();
+	
+	List<DateCountDTO> getWeeklyPostCount();
+	
+	List<DateCountDTO> getWeeklyCommentCount();
+	
+	List<DateCountDTO> getDayConnectedUserCount();
+	
+	List<DateCountDTO> getDayProjectCount();
+	
+	List<DateCountDTO> getDayJobCount();
+	
+	List<DateCountDTO> getDayPostCount();
+	
+	List<DateCountDTO> getDayCommentCount();
+	
+	void insertConnectedUser(Long user_sq);
+	
+	List<LatestPostsDTO> getLatestPosts();
 }
