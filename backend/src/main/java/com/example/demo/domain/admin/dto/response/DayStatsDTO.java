9973434fb78c086c71ac38995457d94de2dabe15
DESKTOP-I05SQM2\신중현 <joungheon15@gmail.com>
2026-03-03 09:30:25 +0900

    feat: 일일 통계 응답 DTO 생성

---
+++ backend/src/main/java/com/example/demo/domain/admin/dto/response/DayStatsDTO.java
@@ -0,0 +1,21 @@
+package com.example.demo.domain.admin.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DayStatsDTO {
+	private String day;
+	private Long visitors;
+	private Long projects;
+	private Long jobs;
+	private Long posts;
+	private Long comments;
+}
