9cbe87de932376996e802908cc90e7d76a05a11b
DESKTOP-I05SQM2\신중현 <joungheon15@gmail.com>
2026-03-05 14:03:57 +0900

    refactor: currentRow 타입을 User에서 AdminUser로 변경

--- frontend/react_js/src/features/users/components/users-provider.tsx
+++ frontend/react_js/src/features/users/components/users-provider.tsx
@@ -1,21 +1,21 @@
 import React, { useState } from 'react'
 import useDialogState from '@/hooks/use-dialog-state'
-import { type User } from '../data/schema'
+import { type AdminUser } from '../data/schema'
 
-type UsersDialogType = 'invite' | 'add' | 'edit' | 'delete'
+type UsersDialogType = 'invite' | 'add' | 'edit' | 'delete' | 'reset-pw'
 
 type UsersContextType = {
   open: UsersDialogType | null
   setOpen: (str: UsersDialogType | null) => void
-  currentRow: User | null
-  setCurrentRow: React.Dispatch<React.SetStateAction<User | null>>
+  currentRow: AdminUser | null
+  setCurrentRow: React.Dispatch<React.SetStateAction<AdminUser | null>>
 }
 
 const UsersContext = React.createContext<UsersContextType | null>(null)
 
 export function UsersProvider({ children }: { children: React.ReactNode }) {
   const [open, setOpen] = useDialogState<UsersDialogType>(null)
-  const [currentRow, setCurrentRow] = useState<User | null>(null)
+  const [currentRow, setCurrentRow] = useState<AdminUser | null>(null)
 
   return (
     <UsersContext value={{ open, setOpen, currentRow, setCurrentRow }}>
