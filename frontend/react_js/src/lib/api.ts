bee30b7cada9c5a505a4630d5432c7b9528d24ac
Analog-PaulBaek <qkdnf325@naver.com>
2026-03-06 15:28:01 +0900

    fix : 운영 환경에 맞게 세팅

--- frontend/react_js/src/lib/api.ts
+++ frontend/react_js/src/lib/api.ts
@@ -6,7 +6,8 @@ import axios, {
 } from 'axios'
 import { useAuthStore } from '@/stores/auth-store'
 
-export const baseUrl = 'http://localhost:8080/api'
+// export const baseUrl = 'http://localhost:8080/api'
+export const baseUrl = 'http://job.estsw.co.kr/api'
 
 interface CustomRequestConfig extends InternalAxiosRequestConfig {
   _retry?: boolean
