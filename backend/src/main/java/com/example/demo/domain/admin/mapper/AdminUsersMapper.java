2abc915dce9084f6e272967e07383caec7d4f525
DESKTOP-I05SQM2\신중현 <joungheon15@gmail.com>
2026-03-06 13:44:46 +0900

    feat: AdminUsersMapper 유저 수정 및 마스터 패스워드 검증 추가

--- backend/src/main/java/com/example/demo/domain/admin/mapper/AdminUsersMapper.java
+++ backend/src/main/java/com/example/demo/domain/admin/mapper/AdminUsersMapper.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.example.demo.common.AmazonS3.UploadedFileDTO;
 import com.example.demo.domain.admin.dto.AdminUsersListDTO;
+import com.example.demo.domain.admin.dto.request.AdminUsersUpdateRequestDTO;
 
 @Mapper
 public interface AdminUsersMapper {
@@ -22,4 +24,33 @@ public interface AdminUsersMapper {
     		@Param("typeCds") List<Long> typeCds,
             @Param("keyword") String keyword,
             @Param("tagKeyword") String tagKeyword);
+    
+    void updateUser(
+    		@Param("userSq") Long userSq,
+    		@Param("dto") AdminUsersUpdateRequestDTO dto,
+    		@Param("encodePw") String encodePw);
+    
+    Long findFileSqByUserSq(
+    		@Param("userSq") Long userSq);
+    
+    String findFileSaveNmByFileSq(
+    		@Param("fileSq") Long fileSq);
+    
+    void updateFileSaveNm(
+    		@Param("fileSq") Long fileSq,
+    		@Param("savedNm") String savedNm);
+    
+    void updateCompanyNm(
+    		@Param("userSq") Long userSq,
+    		@Param("companyNm") String companyNm);
+    
+    void insertFile(
+    		@Param("uploaded") UploadedFileDTO uploaded);
+    
+    Long findFileSqBySavedNm(
+    		@Param("savedNm") String savedNm);
+    
+    void insertUserProfileImage(
+    		@Param("userSq") Long userSq,
+    		@Param("fileSq") Long fileSq);
 }
