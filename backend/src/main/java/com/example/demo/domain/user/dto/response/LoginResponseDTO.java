acaf25200a72f83b9cf04242eb5ff59a685138a6
DESKTOP-ASKOQNA\USER <chxrryda@gmail.com>
2026-03-25 16:55:00 +0900

    refactor : LoginResponseDTO 좌표 타입 Double에서 BigDecimal으로 자료형 변경

--- backend/src/main/java/com/example/demo/domain/user/dto/response/LoginResponseDTO.java
+++ backend/src/main/java/com/example/demo/domain/user/dto/response/LoginResponseDTO.java
@@ -1,6 +1,7 @@
 package com.example.demo.domain.user.dto.response;
 
 import com.example.demo.domain.user.dto.TokenDTO;
+import java.math.BigDecimal;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,8 +20,8 @@ public class LoginResponseDTO {
     private TokenDTO token;
 
     // [추가] 프론트엔드에서 거리 계산의 기준이 될 좌표
-    private Double latitude;
-    private Double longitude;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
 
     // [추가] 유저의 소속 여부 ('Y' 또는 'N')
     private String isAffiliated;
