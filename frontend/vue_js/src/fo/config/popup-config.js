c73408c5f53998ab7ee21be1bc9d945ba6deae29
DESKTOP-KFIDCDF\admin <yurim147@gmail.com>
2026-03-31 15:48:37 +0900

    feat: 팝업 모달창 구현 (공지사항 이동)

---
+++ frontend/vue_js/src/fo/config/popup-config.js
@@ -0,0 +1,6 @@
+export const POPUP_CONFIG = {
+  id: 'freelancer-notice-vue-2026-03',
+  imageUrl: require('@/assets/go-notice-image.png'),
+  linkUrl: 'http://localhost:8504/notice/101',
+  showAgainAfterDays: 1, // 24시간
+}
