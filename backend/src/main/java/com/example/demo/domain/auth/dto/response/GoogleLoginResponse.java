6bd579c9d8a658941a1145bb29cd6e7198683032
DESKTOP-U6M6OLU\ESTSOFT050 <dmsgur7305@naver.com>
2026-09-09 12:03:32 +0900

    fix: 파일 추가

---
+++ backend/src/main/java/com/example/demo/domain/auth/dto/response/GoogleLoginResponse.java
@@ -0,0 +1,26 @@
+package com.example.demo.domain.auth.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class GoogleLoginResponse {
+
+    @JsonProperty("user_sq")
+    private Long userSq;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("user_nm")
+    private String userNm;
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("is_new_user_yn")
+    private String isNewUserYn; // 최초가입 "Y", 재로그인 "N"
+
+}
