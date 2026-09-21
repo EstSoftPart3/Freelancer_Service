5ab59459c77e71b4951df30cab100d16ecef6632
DESKTOP-U6M6OLU\ESTSOFT050 <dmsgur7305@naver.com>
2026-09-22 08:52:47 +0900

    fix : Datatime 수정

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.TimeZone;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -8,10 +10,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
 public class DemoApplication {
+	
+	@PostConstruct
+    public void started() {
+        // JVM 기본 타임존을 한국 시간(KST)으로 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+	
 
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
